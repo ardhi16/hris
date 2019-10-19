b206bf1c8dc10a8a8612eb803867243868b5fd65
achyar10 <achyar.anshorie@gmail.com>
2019-10-19 20:17:12 +0700

    report cuti

---
+++ application/modules/report/views/cuti.php
@@ -0,0 +1,19 @@
+<div class="container-fluid">
+    <div class="card-box">
+        <form action="" method="post">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="">Dari Tanggal</label>
+                        <input type="text" class="form-control datepicker" name="ds" autocomplete="off">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Sampai Tanggal</label>
+                        <input type="text" class="form-control datepicker" name="de" autocomplete="off">
+                    </div>
+                    <button type="submit" class="btn btn-sm btn-success">Download</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
